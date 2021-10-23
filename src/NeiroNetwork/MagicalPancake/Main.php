ede68f59dfee2e6559eefb34ef931baf492c62d2
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-24 01:57:25 +0900

    テンプレートの書き換え

--- src/NeiroNetwork/MagicalPancake/Main.php
+++ src/NeiroNetwork/MagicalPancake/Main.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
-namespace NeiroNetwork\PluginTemplate;
+namespace NeiroNetwork\MagicalPancake;
 
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 
+	protected function onEnable() : void{
+	}
 }
