8e86bda65713c1378d6eafdbdedb600593ce0720
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-24 02:06:11 +0900

    非同期パケット送信ヘルパー みたいなのを

---
+++ src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\MagicalPancake\helper;
+
+class AsyncSendDataPacketHelper{
+
+	private static self $instance;
+
+	public static function getInstance() : self{
+		return self::$instance ?? self::$instance = new self();
+	}
+}
