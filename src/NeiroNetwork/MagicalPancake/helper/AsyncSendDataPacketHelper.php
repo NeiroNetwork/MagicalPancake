c5a1e80ef250dac6e47f5b633b0cc2764781b456
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-24 02:10:38 +0900

    パケット送信に必要なやつを取得していく

--- src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
+++ src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\MagicalPancake\helper;
 
+use pocketmine\network\mcpe\raklib\RakLibInterface;
+use pocketmine\Server;
+use raklib\server\ipc\UserToRakLibThreadMessageSender;
+
 class AsyncSendDataPacketHelper{
 
 	private static self $instance;
 
 	public static function getInstance() : self{
 		return self::$instance ?? self::$instance = new self();
+	}
+
+	private UserToRakLibThreadMessageSender $interface;
+
+	public function __construct(){
+		// grab RakLibInterface::$interface
+		foreach(Server::getInstance()->getNetwork()->getInterfaces() as $interface){
+			if($interface instanceof RakLibInterface){
+				$interface = (new \ReflectionClass($interface))->getProperty("interface");
+				$interface->setAccessible(true);
+				$this->interface = $interface->getValue($interface);
+				break;
+			}
+		}
 	}
 }
