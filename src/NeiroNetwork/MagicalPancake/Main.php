a25ddc2e1e373b768e3efb57e2a1138b5869624c
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-24 10:53:47 +0900

    プレイヤーのセッションIDと座標を取得する

--- src/NeiroNetwork/MagicalPancake/Main.php
+++ src/NeiroNetwork/MagicalPancake/Main.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\MagicalPancake;
 
+use NeiroNetwork\MagicalPancake\helper\AsyncSendDataPacketHelper;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 
 	protected function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents(AsyncSendDataPacketHelper::getInstance(), $this);
 	}
 }
--- src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
+++ src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\MagicalPancake\helper;
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\network\mcpe\compression\ZlibCompressor;
 use pocketmine\network\mcpe\convert\GlobalItemTypeDictionary;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializerContext;
 use pocketmine\network\mcpe\raklib\RakLibInterface;
+use pocketmine\network\mcpe\raklib\RakLibPacketSender;
 use pocketmine\Server;
 use raklib\server\ipc\UserToRakLibThreadMessageSender;
 
-class AsyncSendDataPacketHelper{
+class AsyncSendDataPacketHelper implements Listener{
 
 	private static self $instance;
 
@@ -23,7 +29,11 @@ class AsyncSendDataPacketHelper{
 	private ZlibCompressor $compressor;
 	private UserToRakLibThreadMessageSender $interface;
 
+	private \Volatile $players;
+
 	public function __construct(){
+		$this->players = new \Volatile();
+
 		$this->packetSerializerContext = new PacketSerializerContext(GlobalItemTypeDictionary::getInstance()->getDictionary());
 		$this->compressor = new ZlibCompressor(1, ZlibCompressor::DEFAULT_THRESHOLD, ZlibCompressor::DEFAULT_MAX_DECOMPRESSION_SIZE);
 
@@ -36,5 +46,42 @@ class AsyncSendDataPacketHelper{
 				break;
 			}
 		}
+	}
+
+	public function onJoin(PlayerJoinEvent $event) : void{
+		$player = $event->getPlayer();
+		$session = $player->getNetworkSession();
+
+		// grab NetworkSession::$sender
+		/* cipherを取得しても非同期で動作しない
+		$cipher = (new \ReflectionClass($session))->getProperty("cipher");
+		$cipher->setAccessible(true);
+		$this->cipher = $cipher->getValue($session);
+		*/
+
+		// grab NetworkSession::$sender
+		$sender = (new \ReflectionClass($session))->getProperty("sender");
+		$sender->setAccessible(true);
+		$sender = $sender->getValue($session);
+		/** @var RakLibPacketSender $sender */
+		// grab RakLibPacketSender::$sessionId
+		$sessionId = (new \ReflectionClass($sender))->getProperty("sessionId");
+		$sessionId->setAccessible(true);
+		$sessionId = $sessionId->getValue($sender);
+		/** @var int $sessionId */
+
+		$this->players[$player->getName()]["sessionId"] = $sessionId;
+	}
+
+	public function onQuit(PlayerQuitEvent $event) : void{
+		unset($this->players[$event->getPlayer()->getName()]);
+	}
+
+	public function onMove(PlayerMoveEvent $event) : void{
+		$player = $event->getPlayer();
+		$pos = $event->getTo();
+		$this->players[$player->getName()]["x"] = $pos->x;
+		$this->players[$player->getName()]["y"] = $pos->y;
+		$this->players[$player->getName()]["z"] = $pos->z;
 	}
 }
