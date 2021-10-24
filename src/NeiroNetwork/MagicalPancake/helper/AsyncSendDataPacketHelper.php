f4a3f7f6b40a9aa6ad20c4b818ad1c3a5b2c5ccb
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-24 10:29:49 +0900

    とりあえずコミット

--- src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
+++ src/NeiroNetwork/MagicalPancake/helper/AsyncSendDataPacketHelper.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\MagicalPancake\helper;
 
+use pocketmine\network\mcpe\compression\ZlibCompressor;
+use pocketmine\network\mcpe\convert\GlobalItemTypeDictionary;
+use pocketmine\network\mcpe\protocol\serializer\PacketSerializerContext;
 use pocketmine\network\mcpe\raklib\RakLibInterface;
 use pocketmine\Server;
 use raklib\server\ipc\UserToRakLibThreadMessageSender;
@@ -16,9 +19,14 @@ class AsyncSendDataPacketHelper{
 		return self::$instance ?? self::$instance = new self();
 	}
 
+	private PacketSerializerContext $packetSerializerContext;
+	private ZlibCompressor $compressor;
 	private UserToRakLibThreadMessageSender $interface;
 
 	public function __construct(){
+		$this->packetSerializerContext = new PacketSerializerContext(GlobalItemTypeDictionary::getInstance()->getDictionary());
+		$this->compressor = new ZlibCompressor(1, ZlibCompressor::DEFAULT_THRESHOLD, ZlibCompressor::DEFAULT_MAX_DECOMPRESSION_SIZE);
+
 		// grab RakLibInterface::$interface
 		foreach(Server::getInstance()->getNetwork()->getInterfaces() as $interface){
 			if($interface instanceof RakLibInterface){
