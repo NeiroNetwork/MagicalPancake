9c319a416febe55a991d6a859b528965b19a79f5
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-12 13:52:37 +0900

    とりあえずコミット

--- src/NeiroNetwork/MagicalPancake/player/AsyncPlayer.php
+++ src/NeiroNetwork/MagicalPancake/player/AsyncPlayer.php
@@ -5,22 +5,24 @@ declare(strict_types=1);
 namespace NeiroNetwork\MagicalPancake\player;
 
 use NeiroNetwork\MagicalPancake\helper\AtomicPlayers;
+use NeiroNetwork\MagicalPancake\midi\MidiConverter;
 use pocketmine\scheduler\AsyncTask;
 
 class AsyncPlayer extends AsyncTask{
 
 	private AtomicPlayers $players;
 
-	private bool $shutdown = false;
+	private bool $endMidi = false;
 
-	public function __construct(){
+	public function __construct(private string $file){
 		$this->players = AtomicPlayers::getInstance();
 	}
 
 	public function onRun() : void{
+		$stream = MidiConverter::midiToStream($this->file);
 	}
 
-	public function shutdown() : void{
-		$this->shutdown = true;
+	public function isEnd() : bool{
+		return $this->endMidi;
 	}
 }
