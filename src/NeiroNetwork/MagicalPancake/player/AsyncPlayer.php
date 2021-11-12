9492f3902a49940073d572bf8bf1acda50dec6d7
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-12 09:57:15 +0900

    AsyncPlayer: 関数などを再配置

--- src/NeiroNetwork/MagicalPancake/player/AsyncPlayer.php
+++ src/NeiroNetwork/MagicalPancake/player/AsyncPlayer.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\MagicalPancake\player;
 
+use NeiroNetwork\MagicalPancake\helper\AtomicPlayers;
 use pocketmine\scheduler\AsyncTask;
 
 class AsyncPlayer extends AsyncTask{
 
-	private bool $pause = false;
+	private AtomicPlayers $players;
+
 	private bool $shutdown = false;
+	private bool $pause = false;
+
+	public function __construct(){
+		$this->players = AtomicPlayers::getInstance();
+	}
+
+	public function onRun() : void{
+	}
+
+	public function shutdown() : void{
+		$this->shutdown = true;
+	}
 
 	public function getPause() : bool{
 		return $this->pause;
@@ -17,13 +31,5 @@ class AsyncPlayer extends AsyncTask{
 
 	public function setPause(bool $pause = true) : void{
 		$this->pause = $pause;
-	}
-
-	public function shutdown() : void{
-		$this->shutdown = true;
-	}
-
-	public function onRun() : void{
-		// TODO: Implement onRun() method.
 	}
 }
