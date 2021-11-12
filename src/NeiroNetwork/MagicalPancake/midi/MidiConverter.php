a405b408b15730f3f447f56a7ad75811905e02ec
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-12 13:43:22 +0900

    MidiConverter::noteToPitch() を実装

--- src/NeiroNetwork/MagicalPancake/midi/MidiConverter.php
+++ src/NeiroNetwork/MagicalPancake/midi/MidiConverter.php
@@ -85,4 +85,12 @@ class MidiConverter{
 
 		return $result;
 	}
+
+	public static function noteToPitch(int $note, string $sound = "") : float{
+		$pitches = [1.41, 1.5, 1.59, 1.68333, 1.78, 1.8875, 1 * 2, 1.06 * 2, 1.1225 * 2, 1.19 * 2, 1.26 * 2, 1.33333 * 2];
+		$octaveFixes = ["note.bass" => 2.0, "note.bassattack" => 2.0];
+
+		$octave = 2 ^ (floor($note / 12) - 6);
+		return $pitches[$note % 12] * $octave * ($octaveFixes[$sound] ?? 1.0);
+	}
 }
